15d2508f56148ad5c843164d4184551ebd2eeff0
djlisko01 <djlisko01@gmail.com>
2026-05-02 00:23:55 -0400

    Add settings

---
+++ backend/config.py
@@ -0,0 +1,33 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="../.env", env_file_encoding="utf-8"
+    )
+
+    # Database
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+
+    # App
+    log_level: str = "INFO"
+
+    @property
+    def postgres_url(self) -> str:
+        url = PostgresDsn.build(
+            scheme="postgresql",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+        return str(url)
+
+
+settings = Settings()
