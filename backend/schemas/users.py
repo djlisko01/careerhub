a9513ce5e40419a9a29b4a6bfea4451d6856afa7
djlisko01 <djlisko01@gmail.com>
2026-05-14 19:16:42 -0400

    Add token schema

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,9 +1,16 @@
 from pydantic import BaseModel, ConfigDict
 
 
+class UserLoginSchema(BaseModel):
+    username: str
+    password: str
+
+
 class UserCreateSchema(BaseModel):
     first_name: str
     last_name: str
+    username: str
+    password: str
     linkedin_url: str | None = None
     github_url: str | None = None
 
@@ -11,8 +18,15 @@ class UserCreateSchema(BaseModel):
 class UserUpdateSchema(BaseModel):
     first_name: str | None = None
     last_name: str | None = None
+    username: str | None = None
+    password: str | None = None
     linkedin_url: str | None = None
     github_url: str | None = None
+
+
+class TokenSchema(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
 
 
 class UserReponseSchema(BaseModel):
@@ -23,4 +37,4 @@ class UserReponseSchema(BaseModel):
     last_name: str
     linkedin_url: str | None = None
     github_url: str | None = None
-    active: bool = True
+    active: bool = False
