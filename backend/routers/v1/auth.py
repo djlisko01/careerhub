d7e681ddca0bcce113c69c35328edeb41fe7b651
djlisko01 <djlisko01@gmail.com>
2026-05-14 19:48:02 -0400

    Add get me

--- backend/routers/v1/auth.py
+++ backend/routers/v1/auth.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import jwt
 
-from core.security import create_access_token
+from core.security import create_access_token, decode_access_token
 from db.connectors.sqlalchemy_conn import get_db
 from db.services.user_profile_service import UserService
 from schemas.users import TokenSchema, UserCreateSchema, UserReponseSchema
@@ -13,6 +14,30 @@ router = APIRouter(tags=["auth"])
 def get_user_service(db=Depends(get_db)) -> UserService:
     """Dependency function to provide a `UserService` instance with a database session."""
     return UserService(db=db)
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    user_service: UserService = Depends(get_user_service),
+) -> UserReponseSchema:
+    """Dependency function to retrieve the current authenticated user based on the provided token."""
+    try:
+        payload = decode_access_token(token)
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(
+                status_code=401, detail="Invalid authentication credentials"
+            )
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=401, detail="Invalid authentication credentials"
+        )
+
+    user = user_service.get_user_profile_by_id(int(user_id), raise_err=False)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
 
 
 @router.post("/register", status_code=201)
@@ -38,3 +63,8 @@ def get_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
     return TokenSchema(access_token=create_access_token(subject=user.id))
+
+
+@router.get("/me", response_model=UserReponseSchema)
+def get_me(current_user: UserReponseSchema = Depends(get_current_user)):
+    return current_user
