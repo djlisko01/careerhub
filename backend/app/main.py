79c5a4fe71bb2cf265e39ec01b8474e9798ff7de
djlisko01 <djlisko01@gmail.com>
2026-07-09 12:13:06 -0400

    Add token router to main

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 # Import Routers
 from app.routers import users
+from app.routers import auth
 
 app = FastAPI()
 
 # Include Routers
 app.include_router(users.router)
+app.include_router(auth.router)
 
 @app.get("/health")
 def health():
